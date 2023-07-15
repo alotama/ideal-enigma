bf17efe277cc3dad2126ca6df38e12915cc741ee
Tama Sebastian <sebastian@alotama.com>
2023-07-14 22:04:27 -0300

    feat: maqueta y da estilos a la card de producto

--- app/models/product.js
+++ app/models/product.js
@@ -1,7 +1,20 @@
 import Model, { attr } from '@ember-data/model';
+import { computed } from '@ember/object';
 
 export default class ProductModel extends Model {
   @attr('string') name;
-  @attr('number') price;
+  @attr('string') price;
   @attr('string') code;
+  @attr('string') badge; // por qué funciona esto? solo se muestra en el FE cuando se llama badge, como en Mirage
+  @attr('string') img;
+
+  @computed('price')
+  get priceInPounds() {
+    let pounds = Intl.NumberFormat('en-GB', {
+      style: 'currency',
+      currency: 'GBP',
+    })
+
+    return pounds.format(this.price);
+  }
 }
--- mirage/config.js
+++ mirage/config.js
@@ -8,18 +8,42 @@ export default function () {
           type: 'product',
           id: '1',
           attributes: {
-            name: 'Product 1',
-            price: 100,
+            img: 'images/strawberries.png',
+            name: 'Strawberries',
+            price: 5,
             code: 'GR1',
+            badge: {
+              id: '3_1350',
+              value: '3 for £13.50',
+            },
           },
         },
         {
           type: 'product',
           id: '2',
           attributes: {
-            name: 'Product 2',
-            price: 200,
+            img: 'images/coffee.png',
+            name: 'Coffee',
+            price: 11.23,
             code: 'SR1',
+            badge: {
+              id: 'multibuy',
+              value: 'Multi-buy Discount',
+            },
+          },
+        },
+        {
+          type: 'product',
+          id: '3',
+          attributes: {
+            img: 'images/green-tea.png',
+            name: 'Green Tea',
+            price: 3.11,
+            code: 'SR1',
+            badge: {
+              id: 'multibuy',
+              value: '2 for 1',
+            },
           },
         },
       ],
