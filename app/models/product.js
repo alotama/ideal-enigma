6a3839b9e5dfce28b9110c3ba2a02448449cc788
Tama Sebastian <sebastian@alotama.com>
2023-07-18 16:50:48 -0300

    fix: product card with type row on mobile

--- app/models/product.js
+++ app/models/product.js
@@ -4,7 +4,7 @@ export default class ProductModel extends Model {
   @attr('string') name;
   @attr('string') price;
   @attr('string') code;
-  @attr('string') token; // por qué funciona esto? solo se muestra en el FE cuando se llama badge, como en Mirage
+  @attr('string') token;
   @attr('string') img;
   @attr('number') count;
   @attr('object') promotions;
